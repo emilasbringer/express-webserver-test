5a7550527cf11746c6ee207a9cc37742f778c20b
emilasbringer <emil.asbringer@elev.ga.ntig.segit config --global user.email emil.asbringer@elev.ga.ntig.segit config --global user.email emil.asbringer@elev.ga.ntig.se>
2022-01-21 11:14:09 +0100

    ✨

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
 
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const tasksRouter = require('./routes/tasks');
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../database')
+
+/* 
+    BASE URL /tasks
+    GET / - Get all tasks
+    POST / - Create a new task
+    GET /:id - Get a task by id
+    PUT /:id - Update a task by id
+    DELETE /:id - Delete a task by id
+*/
+
 
 /* GET home page. */
-router.get('/', function(req, res, next)  {
-  res.json({
-      message: 'Hello World!'
-  });
+router.get('/', async(req, res, next)  => {
+  await pool.promise()
+        .query('SELECT * FROM tasks')
+        .then(rows => {
+            console.log(rows);
+            res.json(rows);
+        })
 });
 
 module.exports = router;
